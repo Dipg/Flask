81fb1d5b4ca241d3a57976685803605e1eccf39e
Glitch (estructura-basica-k) <none>
2020-09-26 21:21:47 +0000

    Con botosnes pero a otra pagina

--- server.py
+++ server.py
@@ -69,7 +69,7 @@ app = Flask(__name__)
     
     
     
-@app.route('/products/<string:product_name>')
+@app.route('/products/<string:product_name>',methods = ['POST', 'GET'])
 def getProduct(product_name):
   client = NotionClient(token_v2="112f4d349b7fc9a2e1abbe9b8b7b61f8a7c51c32b38fb9852b50e9ef8ef866c17432e27604969625dc2ef0f545630bbb0ecd09f3fb9a9ca0fe8bfb1a6e8eb9703feffc7454b3544340ed9bbbc48c")
   cv = client.get_collection_view("https://www.notion.so/6079372bb1af43d7abb8f0f7ebb2fc80?v=6a7008cc54b044a0a43942e53acf5cce")
@@ -93,7 +93,42 @@ def getProduct(product_name):
         # return 'hello'
     
     
+
+@app.route('/success/<name>',methods = ['POST', 'GET'])
+def success(name):
+  client = NotionClient(token_v2="112f4d349b7fc9a2e1abbe9b8b7b61f8a7c51c32b38fb9852b50e9ef8ef866c17432e27604969625dc2ef0f545630bbb0ecd09f3fb9a9ca0fe8bfb1a6e8eb9703feffc7454b3544340ed9bbbc48c")
+  cv = client.get_collection_view("https://www.notion.so/6079372bb1af43d7abb8f0f7ebb2fc80?v=6a7008cc54b044a0a43942e53acf5cce")
+  mad=[]
+  for row in cv.collection.get_rows():
+    print(row.Name)
+  
+    if row.Name==name:
+        client2 = NotionClient(token_v2="112f4d349b7fc9a2e1abbe9b8b7b61f8a7c51c32b38fb9852b50e9ef8ef866c17432e27604969625dc2ef0f545630bbb0ecd09f3fb9a9ca0fe8bfb1a6e8eb9703feffc7454b3544340ed9bbbc48c")
+        print(row.url)
+        cv2 = client2.get_collection_view(row.url)
+        print(row.url)
+        promis=[]
+        promis2=[]
+        for row in cv2.collection.get_rows():
+          pro=row.Name
+          pro2=row.value
+          promis.append(pro)
+          promis2.append(pro2)
+        return render_template('index.html', promis=promis,promis2=promis2 )
     
+    
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['nm']
+        user2 = request.form['valor']
+        print(user)
+        return redirect(url_for('success',name= user))
+    else:
+        return render_template('index2.html')
+      
+      
+
     
     
     
@@ -175,38 +210,38 @@ def my_view_func(name):
 
 
 
-@app.route('/success/<name>',methods = ['POST', 'GET'])
-def success(name):
-  if request.method == 'POST':
+# @app.route('/success/<name>',methods = ['POST', 'GET'])
+# def success(name):
+#   if request.method == 'POST':
     
-        client = NotionClient(token_v2="112f4d349b7fc9a2e1abbe9b8b7b61f8a7c51c32b38fb9852b50e9ef8ef866c17432e27604969625dc2ef0f545630bbb0ecd09f3fb9a9ca0fe8bfb1a6e8eb9703feffc7454b3544340ed9bbbc48c")
+#         client = NotionClient(token_v2="112f4d349b7fc9a2e1abbe9b8b7b61f8a7c51c32b38fb9852b50e9ef8ef866c17432e27604969625dc2ef0f545630bbb0ecd09f3fb9a9ca0fe8bfb1a6e8eb9703feffc7454b3544340ed9bbbc48c")
       
-        cv = client.get_collection_view(name)
-        promis=[]
-        promis2=[]
-        for row in cv.collection.get_rows():
-          pro=row.Name
-          pro2=row.value
-          promis.append(pro)
-          promis2.append(pro2)
-        print(promis)
-        print(promis2)
-        return render_template('index.html', promis=promis,promis2=promis2,name2=user2 )
-  return render_template('index.html')
+#         cv = client.get_collection_view(name)
+#         promis=[]
+#         promis2=[]
+#         for row in cv.collection.get_rows():
+#           pro=row.Name
+#           pro2=row.value
+#           promis.append(pro)
+#           promis2.append(pro2)
+#         print(promis)
+#         print(promis2)
+#         return render_template('index.html', promis=promis,promis2=promis2,name2=user2 )
+#   return render_template('index.html')
 
  
 
 
 
-@app.route('/login',methods = ['POST', 'GET'])
-def login():
-    if request.method == 'POST':
-        user = request.form['nm']
-        user2 = request.form['valor']
-        print(user)
-        return redirect(url_for('success',name = user2))
-    else:
-        return render_template('index2.html')
+# @app.route('/login',methods = ['POST', 'GET'])
+# def login():
+#     if request.method == 'POST':
+#         user = request.form['nm']
+#         user2 = request.form['valor']
+#         print(user)
+#         return redirect(url_for('success',name = user2))
+#     else:
+#         return render_template('index2.html')
       
       
 
